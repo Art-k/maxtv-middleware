2779e64d67506da02e681a87f31589b11954f0b6
Artem <artem.kryhin@gmail.com>
2022-03-07 23:41:25 +0200

    add some keys to building

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 ssh root@159.203.47.150 'sudo service maxtv-middleware stop'
 
-ssh root@159.203.47.150 'cd /root/maxtv-middleware/code/; git pull; go build;'
+ssh root@159.203.47.150 'cd /root/maxtv-middleware/code/maxtv-middleware/; git pull; go build;'
+ssh root@159.203.47.150 'mv /root/maxtv-middleware/code/maxtv-middleware/maxtv-middleware /root/maxtv-middleware/maxtv-middleware'
 
 ssh root@159.203.47.150 'sudo service maxtv-middleware start'
 ssh root@159.203.47.150 'sudo service maxtv-middleware status'
